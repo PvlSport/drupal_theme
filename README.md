d1cd98d3973a72235146aaf086e3d4a14b9065e1
Khan <torekhan93@mail.ru>
2018-02-12 10:52:36 +0600

    New custom theme

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a custom theme for assignment.
+It is created on the basis of Zen theme.
+I chose Zen because it much easier to start with Zen than to start with Bartik or Stark.
+
+In the process of development I used mobile first approach in order to create responsive site.
+Also I used Pixel Perfect Plugin for Google Chrome which helped a lot in creating pixel perfect design.
+
+If you want to customize this theme you should install NodeJS version 6.*.* (I used node v6.10.3) because some libraries in package.json don't work on newer version of NodeJS.
+
---
+++ templates/page.tpl.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<header class="header">
+  <div class="header__left">
+    <a href="#" title="Home" rel="home" class="header__logo">
+      <img src="sites/all/themes/sitemade/images-source/oc-logo.png" alt="Home" class="header__logo-image">
+    </a>
+  </div>
+
+  <div class="header__right">
+    <nav>    
+      <ul>
+        <li><a href="#about">About open charity</a></li>
+        <li><a href="#">The blog</a></li>
+      </ul>
+    </nav>
+  </div>
+  <button class="button" >Join Us</button>
+</header>
+
+
+<section class="hero">
+  <div class="bg-image"></div>
+  <div class="content-wrap">  
+    <h1>Sharing ideas for charities</h1>
+    <p>Many charities' goals are similar, as is the functionality we require, but little shared working takes place.</p>
+    <p>By working together, driving shared areas of interest and influencing open source developments we can bring efficiencies, improve the digital experience for our users, and have great impact.</p>
+    <p>Together we can make a bigger difference.</p>
+  </div>
+</section>
+
+<section class="event">
+  <div class="content-wrap">
+    <p class="bold"><span class="text--primary">Next event:</span> June 23<sup>rd</sup> 2016 18:30 - 21:00</p>
+    <p>Cancer Research UK, Angel Building, 407 St John Street, London EC1V 4AD</p>
+  </div>
+  <button class="button" >Register</button>
+</section>
+
+<section class="involved">
+  <div class="content-wrap">
+    <h2>Get involved</h1>
+  </div>  
+  <ul>
+    <li>
+      <div class="card">
+        <div class="card-img">
+          <img src="sites/all/themes/sitemade/images-source/meetup.png" alt="">
+
+        </div>
+        <div class="card-content">
+          <h3 class="card-title">We do meetings</h3>
+          <p>We organise our meetings through the OpenCharity MeetUp group.</p>
+          <button class="button {{modifier_class}}" >Meetup Group</button>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <div class="card-img">
+          <img src="sites/all/themes/sitemade/images-source/slack.png" alt="">
+
+        </div>
+        <div class="card-content">
+          <h3 class="card-title">We communicate</h3>
+          <p>OpenCharity has a slack group for daily communication.</p>
+          <button class="button {{modifier_class}}" >Slack Group</button>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <div class="card-img">
+          <img src="sites/all/themes/sitemade/images-source/share.png" alt="">
+
+        </div>
+        <div class="card-content">
+          <h3 class="card-title">We collaborate</h3>
+          <p>We have a Wiki group set up to share tools and documents.</p>
+          <button class="button {{modifier_class}}" >Wiki Group</button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</section>
+
+<section class="about" id="about">
+  <div class="content-wrap">
+    <h2 class="content-title">Our Mission</h2>
+    <p>Charities and Partners collaborating and sharing open solutions and ideas to create value in the digital space.</p>
+    <p class="bold">If you are a charity or a supplier, we are ready to welcome you.</p>  
+  </div>
+  <ul class="missions">
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/bulb-alt.png" alt="">        
+        <div class="card-content">
+          <h3 class="card-title text--primary">We help Charities</h3>
+          <p>share knowledge and working practice to make the best technology choices.</p>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/together-alt.png" alt="">
+        <div class="card-content">
+          <h3 class="card-title text--primary">We bring together</h3>
+          <p>charities and suppliers to the charity sector to share best practices.</p>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/encourage-alt.png" alt="">
+        <div class="card-content">
+          <h3 class="card-title text--primary">We encourage</h3>
+          <p>collaboration and innovation for the good of the charity sector.</p>
+        </div>
+      </div>
+    </li>
+  </ul>
+  
+  <hr>  
+  
+  <div class="content-wrap">
+    <h2>Our members</h1>
+  </div>    
+  <ul class="partners">
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/Cancer Research.png" alt="">
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/cc.png" alt="">
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/Kop.png" alt="">
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/Zing.png" alt="">
+      </div>
+    </li>
+    <li>
+      <div class="card">
+        <img src="sites/all/themes/sitemade/images-source/comic.png" alt="">
+      </div>
+    </li>
+  </ul>
+  
+  <div class="dots">
+    <div class="dot dot--active"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+  </div>
+</section>
+
+<section class="news">
+  <div class="content-wrap">
+    <h2>Blog</h1>
+  </div>
+  <ul>
+    <li>      
+      <div class="card-content">
+        <h3 class="card-title text--primary">Online Donations Special...</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et quasi velit voluptatum cum maiores exercitationem.</p>
+        <hr>
+        <p>14 Nov 2014
+      </div>      
+    </li>
+    <li>
+      <div class="card-content">
+        <h3 class="card-title text--primary">Online Donations Special...</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et quasi velit voluptatum cum maiores exercitationem.</p>
+        <hr>
+        <p>14 Nov 2014
+      </div>
+    </li>
+    <li>
+      <div class="card-content">
+        <h3 class="card-title text--primary">Online Donations Special...</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et quasi velit voluptatum cum maiores exercitationem.</p>
+        <hr>
+        <p>14 Nov 2014
+      </div>
+    </li>
+    <li>
+      <div class="card-content ">
+        <h3 class="card-title text--primary">Online Donations Special...</h3>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et quasi velit voluptatum cum maiores exercitationem.</p>
+        <hr>
+        <p>14 Nov 2014
+      </div>
+    </li>    
+  </ul>  
+</section>
+
+<footer>
+  <ul>
+    <li>
+      <a href="#">
+        <i style="color:#69c2c6;" class="fab fa-facebook-f fa-2x"></i>      
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i style="color:#69c2c6;" class="fab fa-twitter fa-2x"></i>      
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <i style="color:#69c2c6;" class="fab fa-google-plus-g fa-2x"></i>      
+      </a>
+    </li>
+  </ul>
+
+  <hr>
+
+  <p>This site was built as a collaboration between <span class="text--primary">Manifesto Digital</span> and <span class="text--primary">Compucorp</span></p>
+</footer>
+
+
+<?php print render($page['bottom']); ?>
